f2e7096e80360ecbf7b0d2dea436d7c2ee441ebc
Raduanul Haque <raduanulhaque01@gmail.com>
2025-04-10 22:06:13 +0600

    update 9-4

--- Backend/orderService/src/main/java/com/example/microservices/orderService/services/OrderServices.java
+++ Backend/orderService/src/main/java/com/example/microservices/orderService/services/OrderServices.java
@@ -1,25 +1,36 @@
 package com.example.microservices.orderService.services;
 
+import com.example.microservices.orderService.client.InventoryClient;
 import com.example.microservices.orderService.dto.OrderDTO;
 import com.example.microservices.orderService.model.Order;
 import com.example.microservices.orderService.repository.OrderRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class OrderServices {
-    @Autowired
+
     private OrderRepo orderRepo;
+    private InventoryClient inventoryClient;
 
     public void postOrder(OrderDTO order) {
-        Order saveData = new Order();
-        saveData.setOrderNumber(UUID.randomUUID().toString());
-        saveData.setQuantity(order.getQuantity());
-        saveData.setPrice(order.getPrice());
-        saveData.setSKUCode(order.getSkuCode());
-        System.out.println(order);
-        orderRepo.save(saveData);
+        boolean isProductInStock = inventoryClient.isInStock(order.getSkuCode(),order.getQuantity());
+
+        if(isProductInStock){
+            Order saveData = new Order();
+            saveData.setOrderNumber(UUID.randomUUID().toString());
+            saveData.setQuantity(order.getQuantity());
+            saveData.setPrice(order.getPrice());
+            saveData.setSKUCode(order.getSkuCode());
+            System.out.println(order);
+            orderRepo.save(saveData);
+        }else {
+           throw new RuntimeException("Product with SKU Code"+order.getSkuCode()+"Is out of Stock");
+        }
+
     }
 }
